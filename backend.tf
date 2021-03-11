6cb58b4d8f019c1181ad8a6abbaf2b7a72bf5601
thiagoirch <thiagoirch@gmail.com>
2021-03-11 15:05:22 -0300

    TF1

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = var.region
-    shared_credentials_file = "/home/thiagoirch/.aws/credentials"
+    shared_credentials_file = "/home//.aws/credentials"
     profile = "default"
 }
